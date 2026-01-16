f9fe221451a19c2fa3d0ef07619cc72394c05bb8
tamar <tamar4608@gmail.com>
2026-01-17 00:03:39 +0200

    Remove statsig

--- src/app.tsx
+++ src/app.tsx
@@ -28,26 +28,24 @@ export function App() {
   }, [client]);
 
   return (
-    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
-      <ThemeProvider>
-        <div className="min-h-screen py-2">
-          <Header />
-          <main className="flex flex-1 flex-col items-center justify-center px-4 text-center">
-            <h1 className="font-sans text-6xl font-bold">
-              <div className={`font-rhr text-primary`}>{pageTitle}</div>
-            </h1>
-            <FloatingFormValue />
-            <form className="w-full px-4" onSubmit={e => e.preventDefault()}>
-              <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                <Sections />
-                <CommitAndResetSection />
-                <ConfigSection />
-              </div>
-            </form>
-          </main>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    </StatsigProvider>
+    <ThemeProvider>
+      <div className="min-h-screen py-2">
+        <Header />
+        <main className="flex flex-1 flex-col items-center justify-center px-4 text-center">
+          <h1 className="font-sans text-6xl font-bold">
+            <div className={`font-rhr text-primary`}>{pageTitle}</div>
+          </h1>
+          <FloatingFormValue />
+          <form className="w-full px-4" onSubmit={e => e.preventDefault()}>
+            <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              <Sections />
+              <CommitAndResetSection />
+              <ConfigSection />
+            </div>
+          </form>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
